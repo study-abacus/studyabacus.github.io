b6d4d0e21b98923a29b5b3ecbf0165f72051577b
Jatin Katyal <jatin.katyal13@gmail.com>
2019-02-07 22:50:37 +0530

    result page added

---
+++ result.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    		<link rel="icon" type="image/x-icon" href="images/favicon.png" />
+	    <title>Study Abacus | Learn Vedic Maths | Teacher Training | Start-Up</title>
+	<?php 
+include_once('include_upper.html');
+?>
+</head>
+
+
+<body class="page page-id-857 page-template-default themerex_body body_style_boxed body_filled theme_skin_education article_style_stretch layout_single-standard template_single-standard top_panel_style_light top_panel_opacity_solid top_panel_show top_panel_above menu_right user_menu_show sidebar_hide bg_image_2 wpb-js-composer js-comp-ver-4.6.1 vc_responsive">
+	
+	
+	
+	<div class="body_wrap">
+
+		
+		<div class="page_wrap">
+
+						
+			<div class="top_panel_fixed_wrap"></div>
+
+			<?php
+                include_once("header.html");
+                ?>
+                 <style>
+				.page_top_wrap{
+					background-color:#018763 !important;
+						
+				}
+				
+				</style>
+            	<div class="page_top_wrap page_top_title page_top_breadcrumbs">
+               
+					<div class="content_wrap">
+						<div class="breadcrumbs">
+								<a class="breadcrumbs_item home" href="index.php">Home</a>
+                                <span class="breadcrumbs_delimiter"></span>
+                                <span class="breadcrumbs_item current">Result</span>							
+                        </div>
+<h1 class="page_title" style="color:#fff">Result</h1>
+											</div>
+				</div>
+			
+			<div class="page_content_wrap">
+
+				
+<div class="content">
+<article class="itemscope post_item post_item_single post_featured_default post_format_standard post-756 page type-page status-publish hentry">
+<section class="post_content" itemprop="articleBody"><div class="sc_reviews alignright"><!-- #TRX_REVIEWS_PLACEHOLDER# --></div>
+<div class="vc_row wpb_row vc_row-fluid">
+<div class="wpb_column vc_column_container vc_col-sm-12">
+<div class="wpb_wrapper">
+<div class="sc_section accent_top bg_tint_light" data-animation="animated fadeInUp normal" style="background-color:#f4f7f9;"><div class="sc_section_overlay" style="">
+<div class="sc_section_content">
+<div class="sc_content content_wrap" style="margin-top:2.5em !important;margin-bottom:2.5em !important;">
+
+<h2 class="sc_title sc_title_regular sc_align_center" style="margin-top:0px;margin-bottom:0.85em;text-align:center;">
+    Olympiad Result
+</h2>
+<div class="columns_wrap sc_columns columns_nofluid"><div class="sc_column_item sc_column_item_1"><div class="sc_section" style="font-size:1.25em; line-height: 1.3em; margin-right:5%">
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+            <center>
+                <!-- <form action="">
+                    <input type="text" style="background: white;" placeholder="Your Roll Number">
+                    <button>Submit</button>
+                </form> -->
+                <p>
+                    Result will be announced on 10th February 2019
+                </p>
+            </center>
+		</div> 
+	</div> </div></div></div></div></div></div></div></div></div></div>
+    
+  						
+                            <?php include_once("footer.html"); ?>	<!-- /.contacts_wrap -->
+						
+		</div>	<!-- /.page_wrap -->
+
+	</div>		<!-- /.body_wrap -->
+
+<a href="#" class="scroll_to_top icon-up-2" title="Scroll to top"></a>
+
+<div class="custom_html_section">
+</div>
+
+<?php include_once('include_bottom.html');  ?>
+
+
+</body>
+
+</html>
